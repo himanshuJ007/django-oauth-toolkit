4a7f75564f6cb53371219b7ec9b124ac56e46215
honestbleeps <steve@openingbands.com>
2015-03-25 20:50:15 -0500

    use allowed_scopes if APP_SPECIFIC_SCOPES is enabled

--- oauth2_provider/oauth2_validators.py
+++ oauth2_provider/oauth2_validators.py
@@ -266,7 +266,10 @@ class OAuth2Validator(RequestValidator):
         return set(scopes).issubset(set(oauth2_settings._SCOPES))
 
     def get_default_scopes(self, client_id, request, *args, **kwargs):
-        return oauth2_settings._SCOPES
+        if oauth2_settings.APP_SPECIFIC_SCOPES:
+            return request.client.allowed_scopes
+        else:
+            return oauth2_settings._SCOPES
 
     def validate_redirect_uri(self, client_id, redirect_uri, request, *args, **kwargs):
         return request.client.redirect_uri_allowed(redirect_uri)
